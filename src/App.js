af385ed19f6b920cb1cd852a977b0e1f6996b9a1
Kuldik <112172212+Kuldik@users.noreply.github.com>
2023-11-06 20:02:02 +0000

    fix: 06.11.23

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Counter from "./components/Counter";
+import ClassCounter from './components/ClassCounter';
+import './styles/app.css';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'},
+  ])
+  // Тепеь нужно создать массив, который бдует менять значения постов в замисимости от их
+
   return (
     <div className="App">
-
+      <PostList posts={posts} title="Список постов 1" />
     </div>
   );
 }
+
+// {/* <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>   */}
+// {/* При помощи props теперь можно сделать универсальными назавние, номер, и описани поста */}
+// {/* При написании стилей inline необходимо писать элемент стилей, который состоит из двух и более слов не через дефиз
+// А через принцип JS, то есть следующие слова с заглавной буквы */}
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+  <App/>,
+  // React.createElement('button', {
+  //   onClick: () => {
+  //     console.log('CLICK');
+  //   }
+  // }, 'Click me'),
+  // Для упрощения и улучшения лучшей читаемости кода существует JSX,
+  // Который упрощает написание HTML кода в JS файле
+  // Например:
+  
+  // <button 
+  //   onClick={() => console.log('CLICK!')}>Click me
+  // </button>,
+
+  document.getElementById('root')
+)
